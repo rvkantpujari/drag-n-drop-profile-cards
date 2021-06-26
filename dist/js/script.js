a23d7a00db71edbd7af714520f182c94ce72c23f
rvkantpujari <rvkantpujari@gmail.com>
2021-06-26 09:15:28 +0530

    dark theme feature added

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,3 +3,25 @@ const team = document.querySelector(".team");
 new Sortable(team, {
     animation: 450
 });
+
+// Toggle Switch
+
+let toggleBtn = document.querySelector('#switch');
+
+toggleBtn.addEventListener('change', function() {
+    if(this.checked) {
+        transition();
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+    else {
+        transition();
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+});
+
+let transition = ()=> {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(()=> {
+        document.documentElement.classList.remove('transition');
+    }, 800);
+}
